8e5f2004b3de451a4b5583b125d269dc4b27d8b5
mayaheilpern <chayaheilpern@gmail.com>
2022-02-24 16:21:25 -0500

    post api config

---
+++ client/src/services/postsApiConfig.js
@@ -0,0 +1,26 @@
+import { api } from "./apiConfig";
+
+export const getAllPosts = async () => {
+  const res = await api.get("posts")
+  return res.data
+}
+
+export const createPost = async (data) => {
+  const res = await api.post("posts", { post: data })
+  return res.data
+}
+
+export const getOnePost = async (postid) => {
+  const res = await api.get(`posts/${postid}`)
+  return res.data
+}
+
+export const updatePost = async (postid, data) => {
+  const res = await api.put(`posts/${postid}`, { post: data })
+  return res.data
+}
+
+export const deletePost = async (data) => {
+  const res = await api.delete(`posts/${postid}`, { post: data })
+  return res.data
+}
