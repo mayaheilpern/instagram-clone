f31b867219e2713a4dbf9505635f0de0805982f5
mayaheilpern <chayaheilpern@gmail.com>
2022-02-24 16:01:27 -0500

    signup user funtionality

---
+++ client/src/components/Auth/Register.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signupUser } from "../../services/userApiConfig";
+
+export const Register = () => {
+  const defaultInput = {
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+    message: "",
+  };
+
+  const [input, setInput] = useState(defaultInput);
+  const navigate = useNavigate();
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInput(() => ({
+      ...input,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signupUser(input);
+    setInput(defaultInput);
+    navigate("/");
+  };
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input
+        name="username"
+        placeholder="username"
+        value={input.username}
+        onChange={handleInput}
+      />
+      <input
+        name="email"
+        type="email"
+        placeholder="email"
+        value={input.email}
+        onChange={handleInput}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="password"
+        value={input.password}
+        onChange={handleInput}
+      />
+      <button>Register</button>
+    </form>
+  );
+};
