94c4eb8488d984b4fad86492e3731089dc2afa46
mayaheilpern <chayaheilpern@gmail.com>
2022-02-24 15:02:03 -0500

    verify and migrate comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { verifyUser } from "./services/userApiConfig";
+import { Auth } from "./Screens/Auth";
 
 function App() {
+  const [cuurentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = async = () => {
+      const res = await verifyUser()
+      setCurrentUser(res)
+    }
+    getUser()
+  }, [])
+
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
     </div>
   );
 }
