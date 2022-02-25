f1f4da1aa344fa8d7d5e926f2c193a402e055aa3
mayaheilpern <chayaheilpern@gmail.com>
2022-02-25 15:58:06 -0500

    moved to a dif folder

--- client/src/Screens/Auth.jsx
+++ client/src/Screens/Auth.jsx
@@ -1,23 +1,21 @@
 import { useState } from "react";
 import { Login } from "../components/Auth/Login";
-import { NavTab } from "../components/Auth/NavTab";
+import { NavTab } from "../components/NavTab";
 import { Register } from "../components/Auth/Register";
-import { Tab } from "../components/Auth/Tab";
+import { Tab } from "../components/Tab";
 
 export const Auth = () => {
   const tabs = ["Register", "Login"];
   const [selected, setSelected] = useState(tabs[0]);
 
   return (
-    <div>
-      <NavTab tabs={tabs} selected={selected} setSelected={setSelected}>
-        <Tab isSelected={selected === tabs[0]}>
-          <Register />
-        </Tab>
-        <Tab isSelected={selected === tabs[1]}>
-          <Login />
-        </Tab>
-      </NavTab>
-    </div>
+    <NavTab tabs={tabs} selected={selected} setSelected={setSelected}>
+      <Tab isSelected={selected === tabs[0]}>
+        <Register />
+      </Tab>
+      <Tab isSelected={selected === tabs[1]}>
+        <Login />
+      </Tab>
+    </NavTab>
   );
 };
