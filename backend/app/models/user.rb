13d4efa9de60ca933cc5c87ccd6d82a082ba9ff5
mayaheilpern <chayaheilpern@gmail.com>
2022-02-26 18:47:09 -0500

    typo

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  has_many :drafts, dependent: :destroys
+  has_many :drafts, dependent: :destroy
   has_secure_password
 end
