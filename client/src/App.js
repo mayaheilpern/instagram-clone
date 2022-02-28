9467519f442123858de678f1d29622658c1a5b33
mayaheilpern <chayaheilpern@gmail.com>
2022-02-28 11:21:30 -0500

    deleted landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./Screens/Home";
-import { LandingPage } from "./Screens/LandingPage";
 import { Auth } from "./Screens/Auth";
 
 function App() {
   return (
     <Routes>
       <Route path="/*" element={<Home />} />
-      <Route path="/home" element={<LandingPage />} />
       <Route path="/auth" element={<Auth />} />
     </Routes>
   );
