00b629bac8267eee8296aadeff1b0517c693547b
mayaheilpern <chayaheilpern@gmail.com>
2022-02-27 21:55:24 -0500

    added link to details page

--- client/src/components/Acct/Posts.jsx
+++ client/src/components/Acct/Posts.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export const Posts = ({ posts }) => {
   return (
@@ -6,11 +6,13 @@ export const Posts = ({ posts }) => {
       {posts.map((post) => {
         return (
           <div key={post.id}>
-            <img
-              src={post.image_url}
-              alt={`posts number ${post.id}`}
-              className="w-20 h-20 m-1"
-            />
+            <Link to={`/posts/${post.id}`}>
+              <img
+                src={post.image_url}
+                alt={`posts number ${post.id}`}
+                className="w-20 h-20 m-1"
+              />
+            </Link>
           </div>
         );
       })}
